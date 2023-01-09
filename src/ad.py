52093d5bcb0c621b247b73308eee549fa72af141
Fischer <kevin.fischer@weidmueller.com>
2023-01-09 08:38:19 +0100

    Added nogui variant and argument parser, documentation in progress

--- src/ad.py
+++ src/ad.py
@@ -1,4 +1,5 @@
 from pyad import *
+from treelib import Node, Tree
 
 
 def get_aduser(name: str=None):
@@ -34,3 +35,23 @@ def get_groups_recursive(group: str=None, indices: list=[], rec_depth: int=0):
             for x in temp:
                 groups.append(x)
     return groups
+
+
+def create_tree(tree: Tree=Tree(), parent: Node=None, ad_object=None):
+    if parent is None:
+        name: str = "".join(ad_object.get_attribute("SamAccountName"))
+        tree.create_node(data=name, identifier=name)
+        parent = name
+    memberOf: list = ad_object.get_attribute(attribute="memberOf")
+    for membership in memberOf:
+        group: pyad.adgroup.ADGroup = get_adgroup(membership)
+        name: str = "".join(group.get_attribute("SamAccountName"))
+        id: str = name
+        i: int = 0
+        while tree.get_node(id) is not None:
+            id += f" ({i})"
+            i += 1
+        tree.create_node(data=name, identifier=id, parent=parent)
+        if group.get_attribute("memberOf") is not []:
+            tree = create_tree(tree=tree, parent=id, ad_object=group)
+    return tree
---
+++ src/tree.py
@@ -0,0 +1,88 @@
+import plotly.graph_objects as go
+from igraph import Graph, EdgeSeq
+from src.ad import get_aduser, get_groups, get_groups_recursive
+
+user = get_aduser(input("Enter username: "))
+groups = get_groups(user)
+nested_groups = []
+for group in groups:
+    nested_groups += get_groups_recursive(group)
+print(nested_groups)
+
+def make_annotations(pos, text, font_size=10, font_color='rgb(250,250,250)'):
+    L=len(pos)
+    if len(text)!=L:
+        raise ValueError('The lists pos and text must have the same len')
+    annotations = []
+    for k in range(L):
+        annotations.append(
+            dict(
+                text=labels[k], # or replace labels with a different list for the text within the circle
+                x=pos[k][0], y=2*M-position[k][1],
+                xref='x1', yref='y1',
+                font=dict(color=font_color, size=font_size),
+                showarrow=False)
+        )
+    return annotations
+
+nr_vertices = len(nested_groups)
+v_label = list(map(str, nested_groups))
+G = Graph.Tree(nr_vertices, 2) # 2 stands for children number
+lay = G.layout('rt')
+
+position = {k: lay[k] for k in range(nr_vertices)}
+Y = [lay[k][1] for k in range(nr_vertices)]
+M = max(Y)
+
+es = EdgeSeq(G) # sequence of edges
+E = [e.tuple for e in G.es] # list of edges
+
+L = len(position)
+Xn = [position[k][0] for k in range(L)]
+Yn = [2*M-position[k][1] for k in range(L)]
+Xe = []
+Ye = []
+for edge in E:
+    Xe+=[position[edge[0]][0],position[edge[1]][0], None]
+    Ye+=[2*M-position[edge[0]][1],2*M-position[edge[1]][1], None]
+
+labels = v_label
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=Xe,
+                   y=Ye,
+                   mode='lines',
+                   line=dict(color='rgb(210,210,210)', width=1),
+                   hoverinfo='none'
+                   ))
+fig.add_trace(go.Scatter(x=Xn,
+                  y=Yn,
+                  mode='markers',
+                  name='Group',
+                  marker=dict(symbol='square',
+                                size=12,
+                                color='#6175c1',    #'#DB4551',
+                                line=dict(color='rgb(50,50,50)', width=1)
+                                ),
+                  text=labels,
+                  hoverinfo='text',
+                  opacity=0.8
+                  ))
+
+axis = dict(showline=False, # hide axis line, grid, ticklabels and  title
+            zeroline=False,
+            showgrid=False,
+            showticklabels=False,
+            )
+
+fig.update_layout(title= f'Group memberships of {"".join(user.get_attribute("name"))}',
+              annotations=make_annotations(position, v_label),
+              font_size=12,
+              showlegend=True,
+              xaxis=axis,
+              yaxis=axis,
+              margin=dict(l=40, r=40, b=85, t=100),
+              hovermode='closest',
+              plot_bgcolor='rgb(248,248,248)'
+              )
+fig.show()
