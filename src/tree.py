bfa6a9021d55a729a60666bf2ea271e0787ee9fb
Fischer <kevin.fischer@weidmueller.com>
2023-01-09 14:36:02 +0100

    Provide information about each ADObject

--- src/tree.py
+++ src/tree.py
@@ -1,6 +1,47 @@
 from tkinter import *
 from tkinter import ttk
 from treelib import Tree
+import pyad.adquery
+
+attributes: list = [
+                "canonicalName",
+                "cn",
+                "distinguishedName",
+                "dSCorePropagationData",
+                "groupType",
+                "instanceType",
+                "member",
+                "memberOf",
+                "managedBy",
+                "name",
+                "nTSecurityDescriptor",
+                "ObjectCategory",
+                "ObjectClass",
+                "ObjectGUID",
+                "objectSid",
+                "SamAccountName",
+                "sAMAccountType",
+                "sDRightsEffective",
+                "SIDHistory",
+                "uSNChanged",
+                "uSNCreated",
+                "whenChanged",
+                "whenCreated",
+            ]
+
+
+app = Tk()
+treeview = ttk.Treeview(app)
+info = Text(app, state=DISABLED)
+infos = {}
+
+
+def onClick(event):
+    info.configure(state="normal")
+    info.delete('1.0', END)
+    info.insert(END, chars=infos[treeview.selection()[0]])
+    info.configure(state=DISABLED)
+
 
 def show_tree(tree: Tree=None) -> None:
     """
@@ -11,16 +52,10 @@ def show_tree(tree: Tree=None) -> None:
     """
 
     # Init tkinter
-    app = Tk()
     app.title(f"Groups of {tree.root}")
     app.geometry(f"{app.winfo_screenwidth() - 10}x{app.winfo_screenheight() - 10}+0+0")
-    info = Text(app, state=DISABLED)
     ttk.Label(app, text ="Hierarchical view").pack()
-    treeview = ttk.Treeview(app) 
     treeview.pack(side="left", fill="both", expand=True)
-
-    infos = {} # Future feature
-
     # Create tree nodes in treeview component
     for node in tree.all_nodes():
         parent = node.bpointer
@@ -28,8 +63,18 @@ def show_tree(tree: Tree=None) -> None:
             parent_id = parent
         else:
             parent_id = ""
-        treeview.insert(parent_id, "end", node.identifier, text=node.tag, open=True)
-        infos[node.identifier] = f"Name: {node.tag}"
-    
+
+        query: pyad.adquery.ADQuery = pyad.adquery.ADQuery()
+        query.execute_query(
+            attributes=attributes,
+            where_clause=f"SamAccountName = '{node.data}'"
+        )
+        results = query.get_single_result()
+        infos[node.identifier] = ""
+        for attr in results:
+            infos[node.identifier] += f"{attr}\t:\t\t{results[attr]}\n\n"
+        treeview.insert(parent_id, "end", node.identifier, text=node.tag, open=True, )
+
+    treeview.bind("<ButtonRelease-1>", onClick)
     info.pack(side="right", fill="both", expand=True) 
     app.mainloop()
